4e88d79762e7f2fc13ad0c585833a2544fb2b49e
wangchg <wangchg@hotmail.com>
2018-04-16 16:48:35 +0800

    first commit

--- ui/src/main/java/com/wangchg/example/ui/service/SomeHystrixService.java
+++ ui/src/main/java/com/wangchg/example/ui/service/SomeHystrixService.java
@@ -11,6 +11,7 @@ public class SomeHystrixService {
     @Autowired
     RestTemplate restTemplate;
 
+
     @HystrixCommand(fallbackMethod = "fallbackSome")
     public String getSome() {
         return restTemplate.getForObject("http://some/getsome", String.class);
