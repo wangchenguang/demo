dda459331f2005740493a30cbfcfe0f698edc266
wangchg@hotmail.com <zm85785331>
2018-04-17 04:26:48 +0800

    first commit

--- ui/src/main/java/com/wangchg/example/ui/UiApplication.java
+++ ui/src/main/java/com/wangchg/example/ui/UiApplication.java
@@ -1,9 +1,12 @@
 package com.wangchg.example.ui;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -21,9 +24,24 @@ public class UiApplication {
         SpringApplication.run(UiApplication.class, args);
     }
 
+    @LoadBalanced
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         // Do any additional configuration here
         return builder.build();
+    }
+
+    @Bean
+    public HystrixMetricsStreamServlet hystrixMetricsStreamServlet() {
+        return new HystrixMetricsStreamServlet();
+    }
+
+    @Bean
+    public ServletRegistrationBean registration(HystrixMetricsStreamServlet servlet) {
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean();
+        registrationBean.setServlet(servlet);
+        registrationBean.setEnabled(true);//是否启用该registrationBean
+        registrationBean.addUrlMappings("/hystrix.stream");
+        return registrationBean;
     }
 }
